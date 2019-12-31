c3d14b3c261ca7f9d4edc6cf88cb5ee6eb865691
luis <luisgonzalezyante@gmail.com>
2019-12-31 11:32:14 -0500

    [wip] enabling sensor addition to system

--- app.js
+++ app.js
@@ -5,22 +5,28 @@ const firebase = require('./services/FirebaseService');
 const app = express();
 const PORT = 3000;
 
-// app.use(express.static(__dirname));
-app.use('/', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/js',  express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect JS bootstrap
 app.use('/jquery',  express.static(__dirname + '/node_modules/jquery/dist')); 
 
+// app.get('/', (req, res) => {
+//   res.sendFile("indesx.html");
+// });
 
-app.get('/', (req, res) => {
-  res.sendFile("/index.html");
-});
+// app.get('/sensors', (req, res) => {
+//   res.sendFile("senssors.html");
+// });
 
 app.get('/latest', (req, res) => {
-  const a = firebase.database.collection('aqis').orderBy('timestamp', 'desc').limit(1).get()
+  firebase.database.collection('aqis').orderBy('timestamp', 'desc').limit(1).get()
   .then(querySnapshot => {
     querySnapshot.forEach(doc => res.send(doc.data()));
   }).catch(err => req.send({error: "Failed to retrieve data."}));
+});
+
+app.post('/sensor', (req, res) => {
+  
 });
 
 app.listen(process.env.PORT || PORT, () => {
