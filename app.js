b20b7a311c5032bbaf079681fd9e536825fbce21
Luis Gonzalez-Yante <luisgonzalezyante@gmail.com>
2020-01-06 18:48:41 -0500

    Sensor Registration (#2)

--- app.js
+++ app.js
@@ -1,24 +1,92 @@
 const SensorPollingService = require('./services/SensorPollingService');
 const express = require('express');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const firebase = require('./services/FirebaseService');
 
 const app = express();
-const PORT = 3000;
+dotenv.config();
 
-app.use(express.static(__dirname));
+const PORT = process.env.PORT || 80;
 
-app.get('/', (req, res) => { 
-  res.sendFile(__dirname + "/index.html");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/latest', (req, res) => {
-  const a = firebase.database.collection('aqis').orderBy('timestamp', 'desc').limit(1).get()
-  .then(querySnapshot => {
+app.use(express.static(__dirname + '/public'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js',  express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/jquery',  express.static(__dirname + '/node_modules/jquery/dist'));
+
+app.get('/latest', async (req, res) => {
+  try {
+    const query = firebase.database.collection('aqis');
+    const querySnapshot = await query.orderBy('timestamp', 'desc').limit(1).get()
     querySnapshot.forEach(doc => res.send(doc.data()));
-  }).catch(err => req.send({error: "Failed to retrieve data."}));
+  } catch(err) {
+    req.send({error: "Failed to retrieve data."});
+  }
 });
 
-app.listen(process.env.PORT || PORT, () => {
+app.get('/sensor', async (req, res) => {
+  const getDocs = async (req, key) => {
+    const query = firebase.database.collection('sensors');
+    const snapshot = await query.where(key, "==", req.query[key]).get();
+    const datas = [];
+    snapshot.forEach(doc => {
+      const data = doc.data();
+      data['id'] = doc.id;
+      datas.push(data);
+    });
+    return datas;
+  }
+
+  try {
+    const sensorIDDocs = await getDocs(req, "sensorID");
+    const sensorTypeDocs = await getDocs(req, "sensorType");
+    const sensorNameDocs = await getDocs(req, "sensorName");
+
+    const docs = sensorIDDocs.concat(sensorTypeDocs, sensorNameDocs);
+    const uniqueDocs = [...new Set(docs)];
+
+    res.send(uniqueDocs);
+  } catch(err) {
+    console.log('Failed to search for sensor: ', err);
+    res.sendStatus(400);
+  }
+});
+
+app.post('/sensor', async (req, res) => {
+  try {
+    const snapshot = await firebase.database.collection('sensors')
+      .where("sensorID", "==", req.body.sensorID)
+      .where("sensorName", "==", req.body.sensorName)
+      .where("sensorType", "==", req.body.sensorType)
+      .get();
+
+    if (!snapshot.empty) {
+      console.log('Sensor already exists.');
+      res.sendStatus(404);
+      return;
+    } 
+  } catch(err) {
+    console.log('Failed to search for sensor: ', err);
+    res.sendStatus(400);
+    return;
+  }
+
+  try {
+    const ref = await firebase.database.collection('sensors').add(req.body);
+    console.log('Added document with ID: ', ref.id);
+    res.sendStatus(200);
+    return;
+  } catch(err) {
+    console.log('Failed to add document: ', err);
+    res.sendStatus(400);
+    return;
+  }
+});
+
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
   SensorPollingService.run();
 });
---
+++ public/js/sensors.js
@@ -0,0 +1,57 @@
+$(async () => {
+    $("#addSensorForm button").click(async (e) => {
+        const url = '/sensor';
+        const data = serializeForm("#addSensorForm");
+
+        hideElement("#addSensorAlerts .alert")
+
+        try {
+            await $.post(url, data);
+            showElement("#addSensorAlerts .alert-success");
+        } catch(err) {
+            showElement("#addSensorAlerts .alert-danger");
+        }
+        
+        e.preventDefault();
+    });
+
+    $("#searchSensorForm button").click(async (e) => {
+        const url = '/sensor';
+        const data = serializeForm("#searchSensorForm");
+
+        hideElement("#searchSensorAlerts .alert");
+        hideElement("#sensorSearchResults");
+        $("#sensorSearchResults .results").empty();
+
+        try {
+            const res = await $.get(url, data);
+            res.forEach(doc => {
+                const docElement = `<div class="card mb-5">
+                    <div class="card-body">
+                        <p><b>Sensor Name</b> ${doc.sensorName}</p>
+                        <p><b>Sensor ID</b> ${doc.sensorID}</p>
+                        <p><b>Sensor Type</b> ${doc.sensorType}</p>
+                    </div>
+                </div>`;
+                $("#sensorSearchResults .results").append(docElement);
+            });
+
+            showElement("#sensorSearchResults")
+        } catch(err) {
+            showElement("#searchSensorAlerts .alert-danger")
+        }
+
+        e.preventDefault();
+    });
+});
+
+/* Helper Functions*/
+const hideElement = id => $(id).addClass('d-none');
+const showElement = id => $(id).removeClass('d-none');
+const serializeForm = id => {
+    const formData = $(id).serializeArray();
+    return formData.reduce((acc, val) => {
+        acc[val.name] = val.value;
+        return acc;
+    }, {});
+}
