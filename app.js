0ff232c6a18ed1c2f466dd142d3dc447a7cae142
luis <luisgonzalezyante@gmail.com>
2020-02-01 23:35:06 -0500

    changed access control allow headers

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  // res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   //Auth Each API Request created by user.
   next();
 });
