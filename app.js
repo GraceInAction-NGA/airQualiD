443e3d6cb79dfe3050f87f7d99d7bb19c9efde70
luis <luisgonzalezyante@gmail.com>
2020-02-01 23:25:33 -0500

    using app.use instead of app.all

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 80;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.all('', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
