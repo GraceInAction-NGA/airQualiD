67ef9e004d409bf0b80f09a24069a514b8d7528c
luis <luisgonzalezyante@gmail.com>
2020-02-01 22:36:56 -0500

    updating value for Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.all('', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   //Auth Each API Request created by user.
